6ff8f3c7a06bd71b108613dc8b14bcb5bd022942
Tevin Temu <tevintemu@gmail.com>
2025-12-19 10:48:15 +0300

    rebase

--- src/ecoscope-workflows-ext-ste/ecoscope_workflows_ext_ste/tasks/__init__.py
+++ src/ecoscope-workflows-ext-ste/ecoscope_workflows_ext_ste/tasks/__init__.py
@@ -1,5 +1,48 @@
-from ._example import add_two_thousand
+from ._map_utils import (
+    load_landdx_aoi,
+    download_land_dx,
+    create_map_layers,
+    clean_geodataframe,
+    combine_map_layers,
+    generate_density_grid,
+    build_landdx_style_config,
+    create_view_state_from_gdf,
+    check_shapefile_geometry_type,
+    annotate_gdf_dict_with_geometry_type,
+    create_map_layers_from_annotated_dict,
+    load_map_files,
+    create_layer_from_gdf,
+)
+
+from ._file_ import create_directory
+from ._inspect import view_df  # testing purposes only
+from ._ste_utils import (
+    retrieve_feature_gdf,
+    label_quarter_status,
+    generate_speed_raster,
+    create_seasonal_labels,
+    split_gdf_by_column,
+)
 
 __all__ = [
-    "add_two_thousand",
+    "view_df",
+    "load_map_files",
+    "load_landdx_aoi",
+    "create_directory",
+    "download_land_dx",
+    "create_map_layers",
+    "combine_map_layers",
+    "clean_geodataframe",
+    "split_gdf_by_column",
+    "label_quarter_status",
+    "retrieve_feature_gdf",
+    "generate_speed_raster",
+    "generate_density_grid",
+    "create_layer_from_gdf",
+    "create_seasonal_labels",
+    "build_landdx_style_config",
+    "create_view_state_from_gdf",
+    "check_shapefile_geometry_type",
+    "annotate_gdf_dict_with_geometry_type",
+    "create_map_layers_from_annotated_dict",
 ]
